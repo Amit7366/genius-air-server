71fa2ae9a217476c682a00bf8669724d840734ab
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-10-27 00:44:07 +0600

    modify deatails data by adding bullet point

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+
+const courses = require('./data/courses.json');
+const topics = require('./data/course-details.json');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+  res.send(topics);
+});
+
+app.get('/courses',(req,res) =>{
+    res.send(courses);
+});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
